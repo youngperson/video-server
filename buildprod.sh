fc00229d81a0de6a11141f0b9ea1fd0ccc8ffbc5
万仁亮 <wanrenliang@oneniceapp.com>
2020-04-12 12:35:36 +0800

    all

--- buildprod.sh
+++ buildprod.sh
@@ -2,14 +2,26 @@
 
 # build web and other services
 cd ~/code/src/github.com/video-server/api
-# 目标部署机器的环境设置,编译
-env GOOS=linux GOARCH=amd64 go build -o ../bin/api
+go build -o ../bin/api
 
 cd ~/code/src/github.com/video-server/scheduler
-env GOOS=linux GOARCH=amd64 go build -o ../bin/scheduler
+go build -o ../bin/scheduler
 
 cd ~/code/src/github.com/video-server/streamserver
-env GOOS=linux GOARCH=amd64 go build -o ../bin/streamserver
+go build -o ../bin/streamserver
 
 cd ~/code/src/github.com/video-server/web
-env GOOS=linux GOARCH=amd64 go build -o ../bin/web
+go build -o ../bin/web
+
+
+# ========= 目标部署机器的环境设置,编译(目标机器是Linux)=======
+#env GOOS=linux GOARCH=amd64 go build -o ../bin/api
+
+#cd ~/code/src/github.com/video-server/scheduler
+#env GOOS=linux GOARCH=amd64 go build -o ../bin/scheduler
+
+#cd ~/code/src/github.com/video-server/streamserver
+#env GOOS=linux GOARCH=amd64 go build -o ../bin/streamserver
+
+#cd ~/code/src/github.com/video-server/web
+#env GOOS=linux GOARCH=amd64 go build -o ../bin/web
